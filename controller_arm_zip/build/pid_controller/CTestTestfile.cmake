6820ea72c660e34ea2072ab6ddb87aa20914569e
ESCAPEflash <escape3235@gmail.com>
2023-09-16 17:47:05 +0530

    controller_load is fixed

---
+++ controller_arm_zip/build/pid_controller/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ujjawal/controller/controller_arm_zip/src/pid_controller
+# Build directory: /home/ujjawal/controller/controller_arm_zip/build/pid_controller
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ controller_arm_zip/build/pid_controller/catkin_generated/pkg.installspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "controller_manager".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "pid_controller"
+PROJECT_SPACE_DIR = "/home/ujjawal/controller/controller_arm_zip/install"
+PROJECT_VERSION = "0.0.0"
